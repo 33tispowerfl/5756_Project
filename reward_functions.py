960b4879f234886bd14c67560c2f5a0602c016a3
Biohazard237 <bozhoujin@gmail.com>
2025-05-08 23:35:36 -0400

    reward of an ordinary step is -0.1

--- reward_functions.py
+++ reward_functions.py
@@ -15,7 +15,7 @@ def single_destination(x,y):
         return -50
     if dist(x,y,1.5,1.5)<=0.1:
         return 200
-    return 0
+    return -0.1
 
 # Two destinations 1.5, 1.5 and -1.5, -1.5
 def two_destinations(x,y):
@@ -23,6 +23,7 @@ def two_destinations(x,y):
         return -50
     if dist(x,y,1.5,1.5)<=0.1 or dist(x,y,-1.5,-1.5)<=0.1:
         return 100
+    return -0.1
 
 # Move up. Reward is y if not hit_wall, linearly increasing with y.
 def move_up(x,y):
@@ -42,6 +43,7 @@ def stronger_storm_better_price(x,y):
         return -50
     if x>=1.65 or x<=-1.65 or y>=1.65 or y<= -1.65 or (x<=0.35 and x>=-0.35 and y<=0.2):
         return 50
+    return -0.1
 
 # Rewards the car when the back is pointing towards the closest surrounding wall of the maze (one of walls 1-4). Closer the car is to the wall higher the reward
 def butt_scraper(x,y,theta):
